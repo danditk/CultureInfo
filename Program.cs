3bc04d6ff5af1b537a8b44b497ee40e2cd645796
Darek <darekt@abmicro.pl>
2021-11-09 22:46:23 +0100

    Sings of CurrentCulture as ".", and "-" changed to " @ " and "~MiNuS~".

--- Program.cs
+++ Program.cs
@@ -19,6 +19,15 @@ namespace CultureInfoTool
             //}
 
             Console.WriteLine(CultureInfo.CurrentCulture.Name);
+            // CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator = "@"; wyskakuje problem, bo nie można tak zrobić! Trzeba pracować na kopii.
+            // Poniższe też nie zadziała, bo trzeba wykonać rzutowanie  rzutujący(rzutowanie)
+            //var newCulture = CultureInfo.CurrentCulture.Clone();
+            //newCulture.
+
+            var newCulture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            newCulture.NumberFormat.NumberDecimalSeparator = " @ ";
+            newCulture.NumberFormat.NegativeSign = "~MiNuS~";
+            CultureInfo.CurrentCulture = newCulture; // nadpisanie kultury naszą nową
 
             int a = -5;
             float b = 3.14f; // wartość standardowo wyświetlana z kropką ! Zmienimy to na  małpę @
