7d36b5fdde9fc61bce000ee604e170adb987b535
Darek <darekt@abmicro.pl>
2021-11-09 22:36:31 +0100

    Get current info and current culture name

--- Program.cs
+++ Program.cs
@@ -1,20 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CultureInfo
+namespace CultureInfoTool
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //var allCultureTypes = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            //foreach (var VARIABLE in allCultureTypes)
+            //{
+            //    Console.Write(VARIABLE.Name + " - ");
+            //    Console.WriteLine(VARIABLE.NumberFormat.NumberDecimalSeparator);
+            //}
+
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+
             int a = -5;
             float b = 3.14f; // wartość standardowo wyświetlana z kropką ! Zmienimy to na  małpę @
 
             Console.WriteLine(a);
             Console.WriteLine(b);
+
+            Console.ReadKey();
         }
     }
 }
