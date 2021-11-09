3789adc4a94988b4b94f7a1a903015eda381f79b
Darek <darekt@abmicro.pl>
2021-11-09 22:26:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CultureInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -5;
+            float b = 3.14f; // wartość standardowo wyświetlana z kropką ! Zmienimy to na  małpę @
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
